3d23f92815eae924e87429c64c841f453b7279c8
DavidLiao <davidliao2005@163.com>
2025-06-18 14:50:12 +0800

    added javascript implementation of function calling and benchmarking

---
+++ fc-nodejs/config.js
@@ -0,0 +1,11 @@
+// OpenAI API Configuration
+export const BASE_URL = "https://api.siliconflow.cn/v1";
+export const OPENAI_API_KEY = "sk-wosxiisuzqcpwbnmaobpgflmgxzpumvxsuvusoduscvhcdoc";
+export const MODEL_NAME = "THUDM/GLM-4-9B-0414";
+export const URBAN_DICTIONARY_API_KEY= "your urban dictionary api key";
+export const OPENWEATHERMAP_API_KEY= "your openweathermap api key";
+
+
+// Benchmark Configuration
+export const BENCHMARK_ITERATIONS = 1;
+export const BENCHMARK_RESULTS_DIR = "benchmark_results";
